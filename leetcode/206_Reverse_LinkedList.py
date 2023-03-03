13cb238d4f93953d310985966c707f8e4609c5b4
Thaarik <a.thaarikahamed@gmail.com>
2023-03-03 00:08:35 +0000

    Linkedlist Easy two pointer

---
+++ leetcode/206_Reverse_LinkedList.py
@@ -0,0 +1,47 @@
+'''
+206. Reverse Linked List
+Easy
+
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+Example 2:
+
+
+Input: head = [1,2]
+Output: [2,1]
+Example 3:
+
+Input: head = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the list is the range [0, 5000].
+-5000 <= Node.val <= 5000
+ 
+'''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        #just reverse the next links on each node and assign the head at the end node.
+        prev = None
+        while head:
+            nextn = head.next
+            head.next = prev
+            prev = head
+            head = nextn
+        head = prev
+        return head
