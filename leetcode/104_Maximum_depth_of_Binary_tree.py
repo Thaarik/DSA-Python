f9fc100f3b8362009966aa5400a61d82e6d2c1a0
Thaarik <a.thaarikahamed@gmail.com>
2023-03-10 19:55:49 +0000

    Tree, Iterative and recursive DFS solution

--- leetcode/104_Maximum_depth_of_Binary_tree.py
+++ leetcode/104_Maximum_depth_of_Binary_tree.py
@@ -36,18 +36,32 @@ The number of nodes in the tree is in the range [0, 104].
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         '''using BFFS by finding the length of levels in a tree'''
-        queue = []
-        result = []
-        queue.append(root)
-        while queue:
-            level = []
-            for i in range(len(queue)):
-                node = queue.pop(0)
-                if node:
-                    level.append(node)
-                    queue.append(node.left)
-                    queue.append(node.right)
-            if level:
-                result.append(level)
-        return len(result)
-        
+        # queue = []
+        # result = []
+        # queue.append(root)
+        # while queue:
+        #     level = []
+        #     for i in range(len(queue)):
+        #         node = queue.pop(0)
+        #         if node:
+        #             level.append(node)
+        #             queue.append(node.left)
+        #             queue.append(node.right)
+        #     if level:
+        #         result.append(level)
+        # return len(result)
+        ''' Recursive DFS -find the maximum depth in every sub tree'''
+        # if not root:
+        #     return 0
+        # return 1+max(self.maxDepth(root.left),self.maxDepth(root.right))
+        ''' Iterative DFS using Stack '''
+        stack = [[root,1]] # stack containing the root and its initial depth value
+        result = 0
+        while stack:
+            node,depth = stack.pop() #pop the stack containing node and its root
+            if node:
+                result = max(result,depth) #check the depth value for result
+                stack.append([node.left,depth+1]) #push the node's left and right child with increment in depth
+                stack.append([node.right,depth+1])
+                #repeat the process untill all children's child value is none
+        return result
