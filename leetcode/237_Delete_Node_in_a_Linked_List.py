2bb070199a03584babe00da101f05a1efe2ce378
Thaarik <a.thaarikahamed@gmail.com>
2023-05-24 10:38:54 +0100

    LinkedList Medium

---
+++ leetcode/237_Delete_Node_in_a_Linked_List.py
@@ -0,0 +1,77 @@
+'''
+237. Delete Node in a Linked List
+Medium
+2.9K
+853
+Companies
+There is a singly-linked list head and we want to delete a node node in it.
+
+You are given the node to be deleted node. You will not be given access to the first node of head.
+
+All the values of the linked list are unique, and it is guaranteed that the given node node is not the last node in the linked list.
+
+Delete the given node. Note that by deleting the node, we do not mean removing it from memory. We mean:
+
+The value of the given node should not exist in the linked list.
+The number of nodes in the linked list should decrease by one.
+All the values before node should be in the same order.
+All the values after node should be in the same order.
+Custom testing:
+
+For the input, you should provide the entire linked list head and the node to be given node. node should not be the last node of the list and should be an actual node in the list.
+We will build the linked list and pass the node to your function.
+The output will be the entire list after calling your function.
+ 
+
+Example 1:
+
+
+Input: head = [4,5,1,9], node = 5
+Output: [4,1,9]
+Explanation: You are given the second node with value 5, the linked list should become 4 -> 1 -> 9 after calling your function.
+Example 2:
+
+
+Input: head = [4,5,1,9], node = 1
+Output: [4,5,9]
+Explanation: You are given the third node with value 1, the linked list should become 4 -> 5 -> 9 after calling your function.
+ 
+
+Constraints:
+
+The number of the nodes in the given list is in the range [2, 1000].
+-1000 <= Node.val <= 1000
+The value of each node in the list is unique.
+The node to be deleted is in the list and is not a tail node.
+'''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        pointer1 = node
+        pointer2 = node.next
+        pointer1.val = pointer2.val
+        pointer1.next = pointer2.next
+
+'''Approach:
+1. Since head was not given, we need to delete the given node that are accessible (the self and its next nodes.)
+2. First copy the value of the neighbouring node value and then redirect the given node reference to its neighbour node's next referring node.
+    This will satisfy the above condition.
+
+    i.e., i)  4 -> 5-> 1 -> 9 -> null
+          ii) 4 -> 1 -> 1-> 9 -> null
+          iii)4 -> 1 -> 9 -> null
+                        ^
+                        |
+                        1
+          iv) result 4 -> 1 -> 9 -> null
+'''
