80d750bb6c289e6b2a33a9e79083e125a5b6db90
Thaarik <a.thaarikahamed@gmail.com>
2023-07-01 11:57:09 +0100

    Array Medium

---
+++ leetcode/377_Combination_Sum_IV.py
@@ -0,0 +1,55 @@
+"""
+377. Combination Sum IV
+Medium
+5.8K
+579
+Companies
+Given an array of distinct integers nums and a target integer target, return the number of possible combinations that add up to target.
+
+The test cases are generated so that the answer can fit in a 32-bit integer.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3], target = 4
+Output: 7
+Explanation:
+The possible combination ways are:
+(1, 1, 1, 1)
+(1, 1, 2)
+(1, 2, 1)
+(1, 3)
+(2, 1, 1)
+(2, 2)
+(3, 1)
+Note that different sequences are counted as different combinations.
+Example 2:
+
+Input: nums = [9], target = 3
+Output: 0
+ 
+
+Constraints:
+
+1 <= nums.length <= 200
+1 <= nums[i] <= 1000
+All the elements of nums are unique.
+1 <= target <= 1000
+ 
+"""
+
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        cache = defaultdict(int)
+        def dp(nums,target,cache):
+            if target==0:
+                return 1
+            if target<0:
+                return 0
+            if target in cache:
+                return cache[target]
+            for num in nums:
+                cache[target]+=dp(nums,target-num,cache)
+            return cache[target]
+        return dp(nums,target,cache)
