9046017269217b28f5638483ebb6e4d296432f4c
Thaarik <a.thaarikahamed@gmail.com>
2023-07-29 10:54:50 +0100

    Updated Fibonacci

--- leetcode/509_Fibonacci_Number.py
+++ leetcode/509_Fibonacci_Number.py
@@ -39,3 +39,12 @@ class Solution:
         if n==0 or n==1:
             return n
         return self.fib(n-1)+self.fib(n-2)
+
+        # if n==0:
+        #     return 0
+        # fib=[0]*(n+1)
+        # fib[0]=0
+        # fib[1]=1
+        # for i in range(2,n+1):
+        #     fib[i]=fib[i-1]+fib[i-2]
+        # return fib[n]
