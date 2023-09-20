d0084735f0615317510c82fbbf09cde240f1d391
Thaarik <a.thaarikahamed@gmail.com>
2023-09-20 15:33:00 +0100

    Array Medium

---
+++ leetcode/853_Car_Fleet.py
@@ -0,0 +1,73 @@
+'''
+853. Car Fleet
+Medium
+Topics
+Companies
+There are n cars going to the same destination along a one-lane road. The destination is target miles away.
+
+You are given two integer array position and speed, both of length n, where position[i] is the position of the ith car and speed[i] is the speed of the ith car (in miles per hour).
+
+A car can never pass another car ahead of it, but it can catch up to it and drive bumper to bumper at the same speed. The faster car will slow down to match the slower car's speed. The distance between these two cars is ignored (i.e., they are assumed to have the same position).
+
+A car fleet is some non-empty set of cars driving at the same position and same speed. Note that a single car is also a car fleet.
+
+If a car catches up to a car fleet right at the destination point, it will still be considered as one car fleet.
+
+Return the number of car fleets that will arrive at the destination.
+
+ 
+
+Example 1:
+
+Input: target = 12, position = [10,8,0,5,3], speed = [2,4,1,1,3]
+Output: 3
+Explanation:
+The cars starting at 10 (speed 2) and 8 (speed 4) become a fleet, meeting each other at 12.
+The car starting at 0 does not catch up to any other car, so it is a fleet by itself.
+The cars starting at 5 (speed 1) and 3 (speed 3) become a fleet, meeting each other at 6. The fleet moves at speed 1 until it reaches target.
+Note that no other cars meet these fleets before the destination, so the answer is 3.
+Example 2:
+
+Input: target = 10, position = [3], speed = [3]
+Output: 1
+Explanation: There is only one car, hence there is only one fleet.
+Example 3:
+
+Input: target = 100, position = [0,2,4], speed = [4,2,1]
+Output: 1
+Explanation:
+The cars starting at 0 (speed 4) and 2 (speed 2) become a fleet, meeting each other at 4. The fleet moves at speed 2.
+Then, the fleet (speed 2) and the car starting at 4 (speed 1) become one fleet, meeting each other at 6. The fleet moves at speed 1 until it reaches target.
+ 
+
+Constraints:
+
+n == position.length == speed.length
+1 <= n <= 105
+0 < target <= 106
+0 <= position[i] < target
+All the values of position are unique.
+0 < speed[i] <= 106
+'''
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pair = [(p,s) for p,s in zip(position,speed)]
+        pair.sort(reverse=True) # descending sort of position (p)
+        # print(pair)
+        stack=[]
+        for p,s in pair:
+            stack.append((target-p)/s)
+            # print(stack)
+            while len(stack)>=2 and stack[-1]<=stack[-2]: # top element is lower or equal to the below element
+                stack.pop()
+        # print(stack)
+        return len(stack)
+
+''' Approach:
+1. using Stack.
+2. form a pair of position and speed, and arrange in descending order of position
+3. for each pair, append the value of (target-p)/s inside the stack. 
+4. when length of the stack >=2 and the top most value is less than or equal to the second top most value, pop that tp most value out until it hits the condition. 
+5. Do the following and the remaining left out element, i.e., the length of the final updated stack is the result.
+
+'''
